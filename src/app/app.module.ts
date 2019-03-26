e601b4042130e8e91cff3a08d5ac6c5c4bfc5257
Tobias Seczer <tobias.seczer@gmail.com>
2019-03-26 16:24:00 +0100

    Fixed Errors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import *  as mat from '@angular/material';
+import * as mat from '@angular/material';
 import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -8,7 +8,6 @@ import { MainNavComponent } from './components/lehrer/main-nav-lehrer/main-nav.c
 import { MainNavAdminComponent } from './components/admin/main-nav-admin/main-nav-admin.component';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MainNavSchuelerComponent } from './components/schueler/main-nav-schueler/main-nav-schueler.component';
-import { ProjektComponent } from './components/projekt/projekt.component';
 import { LoginComponent } from './components/login/login.component';
 import { HttpClientModule } from '@angular/common/http';
 
@@ -19,7 +18,6 @@ import { HttpClientModule } from '@angular/common/http';
     MainNavAdminComponent,
     routingComponents,
     MainNavSchuelerComponent,
-    ProjektComponent
     LoginComponent
   ],
   imports: [
@@ -34,7 +32,7 @@ import { HttpClientModule } from '@angular/common/http';
     mat.MatSidenavModule,
     mat.MatIconModule,
     mat.MatListModule,
-    mat.MatCardModule
+    mat.MatCardModule,
     mat.MatFormFieldModule,
     mat.MatInputModule,
     mat.MatButtonModule,
